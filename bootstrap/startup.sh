48b9ad1308ac43640dfd60c7d445963a9894b2e4
Six <23470032+6ixfalls@users.noreply.github.com>
2024-07-16 16:09:50 +0800

    fix: nc wait

--- bootstrap/startup.sh
+++ bootstrap/startup.sh
@@ -1,11 +1,11 @@
 #!/busybox/sh
 set -e -x
 
-cat /etc/teleport/apply-on-startup.yaml
 teleport start --config=/etc/teleport/teleport.yaml --apply-on-startup=/etc/teleport/apply-on-startup.yaml &
 
 echo "Waiting for start"
-sleep 10
+while ! echo exit | nc 127.0.0.1 3025; do sleep 10; done
+echo "Recieved TCP connection"
 echo "Resetting user"
 tctl users reset $ADMIN_USER
 sleep 2
