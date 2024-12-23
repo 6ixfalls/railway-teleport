2d77422fad3f2c3a498f5b3f8c77626db87fd15f
Six <23470032+6ixfalls@users.noreply.github.com>
2024-12-23 00:56:39 -0800

    dont reset if env set

--- bootstrap/startup.sh
+++ bootstrap/startup.sh
@@ -6,8 +6,10 @@ teleport start --config=/etc/teleport/teleport.yaml --apply-on-startup=/etc/tele
 echo "Waiting for start"
 while ! echo exit | nc 127.0.0.1 3025; do sleep 10; done
 echo "Recieved TCP connection"
-echo "Resetting user"
-tctl users reset $ADMIN_USER
+if [ ! -z "$NO_RESET_PASSWORD" ] then
+  echo "Resetting user"
+  tctl users reset $ADMIN_USER
+fi
 sleep 2
 echo "Exiting server"
 exit 0
